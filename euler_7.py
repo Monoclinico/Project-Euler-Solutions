3ea268c769b4d7900d86ba6d09c41763a65f08eb
Matheus Sanches <matheussanchesleme@gmail.com>
2018-09-11 19:00:11 -0300

    Número primo 10001º em 2 segundos!

--- euler_7.py
+++ euler_7.py
@@ -9,29 +9,19 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 """
 
-
-primes = [2]
-n = 3
-q = 0
-met = 0
-t =0
+primes, n  = [2], 3
+q = met = t = 0
 while True:
-  met = n / 2
+  met = n ** (1/2)
   for d in primes:
-    if n % d == 0:
-      q += 1
-    if d > met:
-      t += 1
-    if t == 2:
-      break
-  t = 0
-  if q == 0:
-    primes.append(n)
-  q = 0
+    if n % d == 0: q += 1
+    if d > met: t += 1
+    if t == 2: break
+  if q == 0: primes.append(n)
+  q = t = 0
   n += 2
-  if len(primes) == 10001:
-    break
+  if len(primes) == 10001: break
 print(primes[-1])    
 #In Setup = Intel Core i3 530 2.93 GHz
-#Less time = 59.194545502 seconds
+#Less time = 2.018032216 seconds
 #Answer = 104743
