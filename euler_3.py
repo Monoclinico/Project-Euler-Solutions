2819ae68713a77304bf7ae19263ab6e05536723b
Matheus Sanches <matheussanchesleme@gmail.com>
2018-09-13 18:50:55 -0300

    Largest prime factor solved.

--- euler_3.py
+++ euler_3.py
@@ -2,7 +2,7 @@
 Os fatores primos de 13195 são 5, 7, 13 e 29.
 
 Qual o maior fator primo do número 600851475143?
-
+==================================================
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
